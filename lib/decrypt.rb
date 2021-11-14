a8e9aa85f54fde6b8158bf1cd5a023993a62835e
Robin Dittrich <robinodittrich@gmail.com>
2021-11-14 11:46:42 -0800

    Large scale refactoring. Looking very clean.

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,10 +1,10 @@
 require './lib/enigma'
 
 enigma = Enigma.new
-message = File.readlines(ARGV[0], chomp: true).join(" ")
+message = File.readlines(ARGV[0], chomp: true).join(" ") #Terminal message text file
 output = File.open(ARGV[1], "w") #Terminal command line object 2
-key = ARGV[2]
-date = ARGV[3]
+key = ARGV[2] #Terminal command line object 3
+date = ARGV[3] #Terminal command line object 4
 decryption = enigma.decrypt(message, key, date)
 File.open(output, "w") do |file|
   file.puts decryption[:decryption]
--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,91 +2,25 @@ require 'date'
 require './lib/key_generator'
 require './lib/date_generator'
 require './lib/shift_generator'
+require './lib/encrypt_decryptable'
 
 class Enigma
   include KeyGenerator
   include DateGenerator
   include ShiftGenerator
-  attr_reader :message,
-              :key,
-              :date,
-              :alphabet
-              :return_hash
-  def initialize
-    @alphabet = ("a".."z").to_a << " "
-    @message = "hello world"
-    @key = key
-    @date = date
-    @message_array = [] #breaks every letter into its own string
-  end
+  include EncryptDecryptable
 
   def encrypt(message, key = create_key, date = create_date)
-
-    offset(date) #this works, so why wouldnt the _shift work?
-    final_shifts = final_shifts(key, date)
-
-    encrypted_message = []
-    @message_array = message.downcase.split(//)
-
-    @message_array.each_with_index do |character, index|
-      if index % 4 == 0
-        encr_char_1 = @alphabet.index(character) + (final_shifts[0])
-          encrypted_message << @alphabet.rotate(encr_char_1)[0]
-      elsif index % 4 == 1
-        encr_char_2 = @alphabet.index(character) + (final_shifts[1])
-          encrypted_message << @alphabet.rotate(encr_char_2)[0]
-      elsif index % 4 == 2
-        encr_char_3 = @alphabet.index(character) + (final_shifts[2])
-          encrypted_message << @alphabet.rotate(encr_char_3)[0]
-      elsif index % 4 == 3
-        encr_char_4 = @alphabet.index(character) + (final_shifts[3])
-          encrypted_message << @alphabet.rotate(encr_char_4)[0]
-      else
-        puts "ERROR DURING ENCRYPTION: check your code and try again."
-      end
-    end
-
-    encryption = {
-      :encryption => encrypted_message.join,
-      :key => key,
-      :date => date
-    }
-
-    encryption
-  end
-
-  def decrypt(cypher_text, key = create_key, date = create_date)
-
     offset(date)
     final_shifts = final_shifts(key, date)
+    encrypted_message = encrypt_decryptable(message, final_shifts, "encrypt")
+    {:encryption => encrypted_message.join, :key => key, :date => date}
+  end
 
-    decrypted_message = []
-    cypher_text_array = cypher_text.downcase.split(//)
-
-    cypher_text_array.each_with_index do |character, index|
-      if index % 4 == 0
-        decr_char_1 = @alphabet.index(character) - (final_shifts[0])
-          decrypted_message << @alphabet.rotate(decr_char_1)[0]
-      elsif index % 4 == 1
-        decr_char_2 = @alphabet.index(character) - (final_shifts[1])
-          decrypted_message << @alphabet.rotate(decr_char_2)[0]
-      elsif index % 4 == 2
-        decr_char_3 = @alphabet.index(character) - (final_shifts[2])
-          decrypted_message << @alphabet.rotate(decr_char_3)[0]
-      elsif index % 4 == 3
-        decr_char_4 = @alphabet.index(character) - (final_shifts[3])
-          decrypted_message << @alphabet.rotate(decr_char_4)[0]
-      else
-        puts "ERROR DURING DECRYPTION: check your code and try again."
-      end
-    end
-
-    decryption = {
-      :decryption => decrypted_message.join,
-      :key => key,
-      :date => date
-    }
-
-    decryption
+  def decrypt(message, key = create_key, date = create_date)
+    offset(date)
+    final_shifts = final_shifts(key, date)
+    decrypted_message = encrypt_decryptable(message, final_shifts, "decrypt")
+    {:decryption => decrypted_message.join, :key => key, :date => date}
   end
 end
